1d82c5daee4b937436bee6985ea0950533677029
mac <mac@veryhappythings.co.uk>
2011-04-08 08:45:19 +0100

    Update paths

--- lib/spacegame.rb
+++ lib/spacegame.rb
@@ -1,5 +1,5 @@
 ROOT = File.dirname(File.expand_path(__FILE__))
 
 require 'rubygems'
-require "#{ROOT}/salvage/packet_buffer"
-require "#{ROOT}/salvage/server"
+require "#{ROOT}/spacegame/packet_buffer"
+require "#{ROOT}/spacegame/server"
